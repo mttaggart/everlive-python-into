f717ea2b612b84d1196b5eb92483169579b6921d
mttaggart <michael.t.taggart@gmail.com>
2020-02-03 20:57:12 -0800

    swapi.py

---
+++ 4-3/swapi.py
@@ -0,0 +1,20 @@
+"""
+SWAPI-CLI: Command-Line interface to the Star Wars API
+"""
+import requests
+import argparse
+
+# Define constants
+
+# Define endpoints
+
+def search(query):
+    """Query the correct API endpoint and return a list of results."""
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
