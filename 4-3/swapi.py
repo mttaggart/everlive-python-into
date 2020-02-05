454634915502c734a4a7562b6dc8b73a758f71b8
mttaggart <michael.t.taggart@gmail.com>
2020-02-05 06:05:46 -0800

    Student 4-3 starter

--- 4-3/swapi.py
+++ 4-3/swapi.py
@@ -1,18 +1,41 @@
 """
 SWAPI-CLI: Command-Line interface to the Star Wars API
 """
-import requests
 import argparse
+import requests
+import csv
 
 # Define constants
 
 # Define endpoints
 
-def search(query):
+class Result:
+    # Constructor
+
+    # String representation
+    pass
+
+class Csv:
+
+    # Constructor
+
+    # Static methods for headers/rows
+
+    # Write CSV
+    pass
+
+def search(query_type, query_string):
     """Query the correct API endpoint and return a list of results."""
     pass
 
 def main():
+    """Run the program"""
+
+    # Create the parser
+    # Parse arguments
+    # Retrieve results
+   
+    # CSV if set, otherwise print normally
     pass
 
 
