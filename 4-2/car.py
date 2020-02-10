2c8bef31e58cbef526786343ac97839082a83ce5
mttaggart <michael.t.taggart@gmail.com>
2020-02-09 21:54:50 -0800

    4-2 starters

---
+++ 4-2/car.py
@@ -0,0 +1,10 @@
+class Car:
+    
+    def __init__(self, make, model, top_speed, acceleration):
+        pass
+
+    def __str__(self):
+        pass
+
+    def advance(self):
+        pass
---
+++ 4-2/race.py
@@ -0,0 +1,12 @@
+from car import Car
+
+class Race:
+
+    def __init__(self, racers, distance):
+        pass
+
+    def tick(self):
+        pass
+
+    def run(self):
+        pass
