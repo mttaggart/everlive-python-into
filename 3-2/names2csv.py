2c9d712a7d9304dde91310f176c89561bee56144
mttaggart <michael.t.taggart@gmail.com>
2020-02-09 23:39:59 -0800

    3-2/names2csv starter

---
+++ 3-2/names2csv.py
@@ -0,0 +1,19 @@
+import sys
+import csv
+
+# Set up CL args
+
+# Define header
+
+def main():
+    rows = []
+    # Read in rows
+    # Process rows
+    # Write rows to file
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
